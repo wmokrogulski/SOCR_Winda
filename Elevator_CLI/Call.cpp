508fbac33d4c7c0d2a118cd5484aa39b597a3022
Wawrzyniec Mokrogulski <mokrogulski1b@gmail.com>
2020-11-10 18:35:22 +0100

    Added Call class

--- Elevator_CLI/Call.cpp
+++ Elevator_CLI/Call.cpp
@@ -1,1 +1,13 @@
 #include "Call.h"
+
+Call::Call(int floor)
+{
+}
+
+Call::Call(int floor, double weight)
+{
+}
+
+Call::Call(int floor, double weight, string name)
+{
+}
--- Elevator_CLI/Call.h
+++ Elevator_CLI/Call.h
@@ -1,5 +1,17 @@
 #pragma once
+#include <string>
+
+using namespace std;
+
 class Call
 {
+private:
+	string name;
+	double weight;
+	int floor;
+public:
+	Call(int floor);
+	Call(int floor, double weight);
+	Call(int floor, double weight, string name);
 };
 
